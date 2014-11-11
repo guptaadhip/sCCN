5349894c8e30d93eb532954a0c80fd2aa67e9fa5
Adhip Gupta <adhipgup@usc.edu>
2014-11-10 18:52:05 -0800

    Packet Engine Definition file

---
+++ include/PacketEngine.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+
+class PacketEngine {
+ public:
+  PacketEngine(std::string interface, unsigned int id);
+  /*
+   * Send Packet Function
+   */
+  void send(char *msg, unsigned int size);
+
+  /*
+   * Receive Packet Function
+   */
+  void receive(char *msg);
+
+  /*
+   * Forward Packet Function (Switch)
+   */
+  void forward(char *msg, unsigned int size);
+
+  /*
+   * Get Interface name
+   */
+  std::string getInterface() const;
+
+  /*
+   * Get my id
+   */
+  unsigned int getId() const;
+
+ private:
+  std::string interface_;
+  unsigned int id_;
+}
