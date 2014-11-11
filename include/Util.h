1e60e5d999d94db877e65b4ed4b320b1baf7c28e
Adhip Gupta <adhipgup@usc.edu>
2014-11-11 11:34:12 -0800

    UniqueID Generation code added to util.h

--- include/Util.h
+++ include/Util.h
@@ -4,6 +4,9 @@
 #include <limits>
 #include <ctime>
 
+/*
+ * Random sequence number generator based on the time
+ */
 unsigned int sequenceNumberGen() {
   typedef boost::mt19937 RNGType;
   RNGType rng;
@@ -13,4 +16,16 @@ unsigned int sequenceNumberGen() {
   boost::variate_generator< RNGType, boost::uniform_int<unsigned int> > 
                             sequenceNo(rng, seqNo);
   return (unsigned int) sequenceNo();
+}
+
+/*
+ * Unique Id Generator (minimum unique ID is 100) 
+ * for the first time we need to pass 0 as the last unique ID
+ */
+unsigned int uniqueIdGenerator(unsigned int lastUniqueId) {
+  if (lastUniqueId == 0) {
+    return 100;
+  } else {
+    return lastUniqueId++;
+  }
 }
