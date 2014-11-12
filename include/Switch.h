f7bc97c828932e92c891701a6eda2280f1677678
Apoorv Bhargava <apoorvbhargava89@gmail.com>
2014-11-12 03:51:27 -0800

    Added switch files to send hello packet; Not tested

---
+++ include/Switch.h
@@ -0,0 +1,23 @@
+#include <string>
+#include <unordered_map>
+#include "include/MyInterfaces.h"
+#include "include/PacketEngine.h"
+
+class Switch{
+ public:
+  Switch(unsigned int switchId);
+  /*Send hello*/
+  void sendHello();
+  /*Send "I am up" message*/
+  void sendImUp();
+
+ private:
+  std::unordered_map<std::string, PacketEngine> ifToPacketEngine;
+  MyInterfaces myInterface_;
+  unsigned int switchId_;
+};
+   
+    
+
+
+  
---
+++ src/Switch.cpp
@@ -0,0 +1,44 @@
+#include "include/Switch.h"
+#include "include/PacketEngine.h"
+#include <iostream>
+#include <string>
+#include "include/Logger.h"
+#include "include/Util.h"
+#include "include/net.h"
+#include <unistd.h>
+
+using namespace std;
+
+Switch::Switch(unsigned int switchId){
+  switchId_ = switchId;
+  Logger::log(Log::DEBUG, __FUNCTION__, __LINE__, 
+              "Inside Switch constructor");
+  for(auto &interface : myInterface_.getInterfaceList()) {
+    PacketEngine packetEngine(interface, switchId);
+    std::pair<std::string, PacketEngine> ifPePair (interface, packetEngine); 
+    ifToPacketEngine.insert(ifPePair);
+    /*for(auto it = myMap.cbegin(); it != myMap.cend(); ++it)
+    {
+          std::cout << it->first << " " << it->second.first << " " << it->second.second << "\n";
+    }*/
+  }
+}
+
+void Switch::sendHello() {
+  char helloPacket[BUFLEN];
+  memset(helloPacket,'\0',BUFLEN);
+  struct HelloPacketHeader helloPac;
+  memset(helloPacket,'\0',BUFLEN);
+  PacketType pacType = PacketType::HELLO;
+
+  helloPac.nodeId=switchId_;
+
+  memcpy(helloPacket, &pacType, sizeof(unsigned short));
+  memcpy(helloPacket + sizeof(unsigned short), &helloPac, sizeof(struct HelloPacketHeader));
+  for (auto &entry : ifToPacketEngine) {
+    entry.second.send(helloPacket, BUFLEN);
+  }
+  
+}
+
+
