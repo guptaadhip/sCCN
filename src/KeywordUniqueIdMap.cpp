a38e4b684720d9a703de71029602f88881a7ba37
Dhanya Davies <dhanya.davies@gmail.com>
2014-11-13 21:25:50 -0800

    Added-keyword uniqueID map class

---
+++ src/KeywordUniqueIdMap.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include "include/KeywordUniqueIdMap.h"
+
+using namespace std;
+
+/*
+*	keywordExists - Check if that keyword exists in the map
+*/
+bool KeywordUniqueIdMap::keywordExists(string keyword)
+{
+	/*check if the keyword is present in map*/
+	if (keywordID_.count(keyword)>0)
+		return true;
+	else
+		return false;
+}
+
+/*
+* addKeywordIDPair - Adds a keyword- UniqueID pair to the map
+*/
+bool KeywordUniqueIdMap::addKeywordIDPair(string keyword, unsigned int uniqueID)
+{
+	/*check if the pair exists already. If yes, return false*/
+	if(keywordExists(keyword))
+		return false;
+	/*create a pair of the keyword and the unique ID*/
+	pair<string,unsigned int> newPair(keyword,uniqueID);
+	/*insert this into the map*/
+	keywordID_.insert(newPair);
+	return true;
+}
+
+/*
+* removeKeywordIDPair - Removes a keyword- UniqueID pair on basis of keyword
+*/
+bool KeywordUniqueIdMap::removeKeywordIDPair(string keyword)
+{
+	/*if keyword is not present in map, return false*/
+	if(!keywordExists(keyword))
+		return false;
+	/*remove the keyword uniqueID pair*/
+	keywordID_.erase(keyword);
+	return true;
+}
+
+/*
+* fetchUniqueID - Fetches Unique ID on basis of keyword
+*/
+unsigned int KeywordUniqueIdMap::fetchUniqueID(string keyword)
+{
+	/*if the required keyword does not exist, return 0*/
+	if(!keywordExists(keyword))
+		return 0;
+	return keywordID_[keyword];
+}
+
+/*
+* fetchUniqueID - Fetches Unique ID on basis of keyword
+*/
+string KeywordUniqueIdMap::fetchKeyword(unsigned int uniqueID)
+{
+	/*if the required keyword does not exist, return 0*/
+	for ( auto it = keywordID_.begin(); it!= keywordID_.end(); ++it )
+    if(it->second == uniqueID)
+			return it->first;
+	return "";
+}
+
+/*
+* displayMap - Print all the data from the map
+*/
+void KeywordUniqueIdMap::displayMap()
+{
+	/*display the entire map*/
+	for ( auto it = keywordID_.begin(); it!= keywordID_.end(); ++it )
+    std::cout <<  it->first << ": " << it->second << endl;
+}
