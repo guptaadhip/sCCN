9b2f8544809404a15041e2ebf424c0ceb172e58d
Adhip Gupta <adhipgup@usc.edu>
2014-11-14 02:08:31 -0800

    Realigning the code to proper standards

--- include/KeywordUniqueIdMap.h
+++ include/KeywordUniqueIdMap.h
@@ -2,38 +2,42 @@
 #include <unordered_map>
 #include <string>
 
-/* Ip Table Entry Object */
-class KeywordUniqueIdMap {
-  public:
-	/*
-	*	keywordExists - Check if that keyword exists in the map
-	*/
-	bool keywordExists(std::string keyword);
+/* 
+ * Keyword to Unique Id Map Class
+ */
 
-	/*
-	* addKeywordIDPair - Adds a keyword- UniqueID pair to the map
-	*/
-	bool addKeywordIDPair(std::string keyword,unsigned int uniqueID);
-  
-	/*
-	* removeKeywordIDPair - Removes a keyword- UniqueID pair on basis of keyword
-	*/
+class KeywordUniqueIdMap {
+ public:
+  /*
+   * keywordExists - Check if that keyword exists in the map
+   */
+  bool keywordExists(std::string keyword);
+
+  /*
+   * addKeywordIDPair - Adds a keyword- UniqueID pair to the map
+   */
+  bool addKeywordIDPair(std::string keyword,unsigned int uniqueID);
+
+  /*
+   * removeKeywordIDPair - Removes a keyword- UniqueID pair on basis of keyword
+   */
   bool removeKeywordIDPair(std::string keyword);
+
+  /*
+   *fetchUniqueID - Fetches Unique ID on basis of keyword
+   */
+  unsigned int fetchUniqueID(std::string keyword);
+
+  /*
+   * fetchKeyword - Fetches Keyword on basis of Unique ID
+   */
+  std::string fetchKeyword(unsigned int uniqueID);
+   
+  /*
+   * displayMap - Print all the data from the map
+   */
+  void displayMap();
 	
-	/*
-	* fetchUniqueID - Fetches Unique ID on basis of keyword
-	*/
-	unsigned int fetchUniqueID(std::string keyword);
-	
-	/*
-	* fetchKeyword - Fetches Keyword on basis of Unique ID
-	*/
-	std::string fetchKeyword(unsigned int uniqueID);
-	/*
-	* displayMap - Print all the data from the map
-	*/
-	void displayMap();
-	
-  private:
-    std::unordered_map<std::string, unsigned int> keywordID_;
+ private:
+  std::unordered_map<std::string, unsigned int> keywordID_;
 };
--- src/KeywordUniqueIdMap.cpp
+++ src/KeywordUniqueIdMap.cpp
@@ -1,77 +1,98 @@
-#include <iostream>
 #include "include/KeywordUniqueIdMap.h"
+#include <cstring>
+#include "include/Logger.h"
 
 using namespace std;
 
 /*
-*	keywordExists - Check if that keyword exists in the map
-*/
-bool KeywordUniqueIdMap::keywordExists(string keyword)
-{
-	/*check if the keyword is present in map*/
-	if (keywordID_.count(keyword)>0)
-		return true;
-	else
-		return false;
+ * keywordExists - Check if that keyword exists in the map
+ */
+bool KeywordUniqueIdMap::keywordExists(string keyword) {
+  /*
+   * check if the keyword is present in map
+   */
+  if (keywordID_.count(keyword) > 0) { 
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /*
-* addKeywordIDPair - Adds a keyword- UniqueID pair to the map
-*/
-bool KeywordUniqueIdMap::addKeywordIDPair(string keyword, unsigned int uniqueID)
-{
-	/*check if the pair exists already. If yes, return false*/
-	if(keywordExists(keyword))
-		return false;
-	/*create a pair of the keyword and the unique ID*/
-	pair<string,unsigned int> newPair(keyword,uniqueID);
-	/*insert this into the map*/
-	keywordID_.insert(newPair);
-	return true;
+ * addKeywordIDPair - Adds a keyword- UniqueID pair to the map
+ */
+bool KeywordUniqueIdMap::addKeywordIDPair(string keyword, unsigned int uniqueID) {
+	/*
+   * check if the pair exists already. If yes, return false
+   */
+  if(keywordExists(keyword)) {
+    return false;
+  }
+	/*
+   * create a pair of the keyword and the unique ID
+   */
+  pair<string,unsigned int> newPair(keyword,uniqueID);
+	/*
+   * insert this into the map
+   */
+  keywordID_.insert(newPair);
+  return true;
 }
 
 /*
-* removeKeywordIDPair - Removes a keyword- UniqueID pair on basis of keyword
-*/
-bool KeywordUniqueIdMap::removeKeywordIDPair(string keyword)
-{
-	/*if keyword is not present in map, return false*/
-	if(!keywordExists(keyword))
-		return false;
-	/*remove the keyword uniqueID pair*/
-	keywordID_.erase(keyword);
-	return true;
+ * removeKeywordIDPair - Removes a keyword- UniqueID pair on basis of keyword
+ */
+bool KeywordUniqueIdMap::removeKeywordIDPair(string keyword) {
+  /*
+   * if keyword is not present in map, return false
+   */
+  if(!keywordExists(keyword)) {
+    return false;
+  }
+  /*
+   * remove the keyword uniqueID pair
+   */
+  keywordID_.erase(keyword);
+  return true;
 }
 
 /*
-* fetchUniqueID - Fetches Unique ID on basis of keyword
-*/
-unsigned int KeywordUniqueIdMap::fetchUniqueID(string keyword)
-{
-	/*if the required keyword does not exist, return 0*/
-	if(!keywordExists(keyword))
-		return 0;
-	return keywordID_[keyword];
+ * fetchUniqueID - Fetches Unique ID on basis of keyword
+ */
+unsigned int KeywordUniqueIdMap::fetchUniqueID(string keyword) {
+  /*
+   * if the required keyword does not exist, return 0
+   */
+  if(!keywordExists(keyword)) {
+    return 0;
+  }
+  return keywordID_[keyword];
 }
 
 /*
-* fetchUniqueID - Fetches Unique ID on basis of keyword
-*/
-string KeywordUniqueIdMap::fetchKeyword(unsigned int uniqueID)
-{
-	/*if the required keyword does not exist, return 0*/
-	for ( auto it = keywordID_.begin(); it!= keywordID_.end(); ++it )
-    if(it->second == uniqueID)
-			return it->first;
-	return "";
+ * fetchUniqueID - Fetches Unique ID on basis of keyword
+ */
+string KeywordUniqueIdMap::fetchKeyword(unsigned int uniqueID) {
+  /*
+   * if the required keyword does not exist, return 0
+   */
+  for (auto &entry : keywordID_) {
+    if(entry.second == uniqueID) {
+      return entry.first;
+    }
+  }
+  return "";
 }
 
 /*
-* displayMap - Print all the data from the map
-*/
-void KeywordUniqueIdMap::displayMap()
-{
-	/*display the entire map*/
-	for ( auto it = keywordID_.begin(); it!= keywordID_.end(); ++it )
-    std::cout <<  it->first << ": " << it->second << endl;
+ * displayMap - Print all the data from the map
+ */
+void KeywordUniqueIdMap::displayMap() {
+  /*
+   * display the entire map
+   */
+  for (auto &entry : keywordID_) {
+    Logger::log(Log::INFO, __FILE__, __FUNCTION__, __LINE__,
+                entry.first + ": " + std::to_string(entry.second));
+  }
 }
