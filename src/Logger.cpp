441f07d009e384f08eec91e70dffe0dd4ee611d4
Adhip Gupta <adhipgup@usc.edu>
2014-11-14 14:04:16 -0800

    Logger minior cosmetic fix

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -6,16 +6,16 @@ bool DEBUG_MODE = false;
 void Logger::log(Log logType, std::string fileName, 
                     std::string function, int line, std::string msg) {
   if (logType == Log::CRITICAL) {
-    std::cout << "CRITICAL: " << fileName << function << "::" 
+    std::cout << "CRITICAL: " << fileName << ": " << function << "::" 
               << line << ": " << msg << std::endl;
     exit(1);
   } else if (logType == Log::DEBUG) {
     if (DEBUG_MODE) {
-      std::cout << "DEBUG: " << fileName << function << "::" 
+      std::cout << "DEBUG: " << fileName << ": " << function << "::" 
                 << line << ": " << msg << std::endl;
     }
   } else if (logType == Log::WARN) {
-    std::cout << "WARN: " << fileName << function << "::" 
+    std::cout << "WARN: " << fileName << ": " << function << "::" 
               << line << ": " << msg << std::endl;
   }
 }
