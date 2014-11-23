adf963a1a1103e7f8e79724b6ce24b3d9e7b8e08
Adhip Gupta <adhipgup@usc.edu>
2014-11-23 14:38:40 -0800

    Host: Interface python

---
+++ hostInterface.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import socket
+import struct
+import os, os.path
+
+def printHelp():
+  print "Commands:"
+  print "\tquit"
+  print "\thelp"
+  print "\tsubscribe"
+  print "\tpublish"
+  print "\tshow publish keyword list"
+  print "\tshow subscribe keyword list"
+  print "\tsend data"
+
+def getKeywords():
+  keywords = []
+  print "Input keyword (one keyword per line, when done enter 'q')"
+  count = 1
+  while True: 
+    try:
+      x = raw_input( str(count) + " " )
+      x = x.strip()
+      if "q" == x:
+        strKeywords = ';'.join(keywords)
+        strKeywords += ';'
+        return strKeywords, len(strKeywords)
+      keywords.append(x)
+      count = count + 1
+      continue
+    except KeyboardInterrupt, k:
+      return 
+
+def publish():
+  keywords, listLen = getKeywords()
+  if not keywords:
+    print "No keywords founds"
+    return
+  data = []
+  data.append("p")
+  data.append(listLen)
+  data.append(keywords)
+  print keywords, listLen
+  s = struct.Struct('cIs')
+  payload = s.pack(*data)
+  return payload
+
+def subscribe():
+  keywords, listLen = getKeywords()
+  if not keywords:
+    print "No keywords founds"
+    return
+  data = []
+  data.append("s")
+  data.append(listLen)
+  data.append(keywords)
+  print keywords, listLen
+  s = struct.Struct('cIs')
+  payload = s.pack(*data)
+  return payload
+
+print "Connecting..."
+#if os.path.exists( "/tmp/hostSocket" ):
+if True:
+  #client = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
+  #client.connect( "/tmp/hostSocket" )
+  print "Ready."
+  printHelp()
+  while True:
+    try:
+      x = raw_input( "> " )
+      x = x.strip()
+      if "" != x:
+        if "help" == x:
+          printHelp()
+          continue
+        if "publish" == x:
+          data = publish()
+          #client.send(data)
+          continue
+        if "subscribe" == x:
+          data = subscribe()
+          #client.send(data)
+          continue
+        if "send data" == x:
+          data = sendData()
+          #client.send(data)
+          continue
+        client.send( x ) 
+        if "quit" == x:
+          print "Shutting down."
+          break
+        data = client.recv(2048);
+        if x == "show publish keyword list":
+          print "Publishing data for: \n" + data
+        elif x == "show subscribe keyword list":
+          print "Subscribed for data: \n" + data
+        else:
+          print "Invalid command";
+    except KeyboardInterrupt, k:
+      print "Shutting down."
+  client.close()
+else:
+  print "Couldn't Connect!"
+  exit(1)
+print "Done"
