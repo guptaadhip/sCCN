eb723c69e9a0a5fb349a056bb47d187aa1020cf1
Adhip Gupta <adhipgup@usc.edu>
2014-12-08 19:13:32 -0800

    Initialization Script: deter

---
+++ sccnInit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt-get update
+apt-get install tcpdump -y
+apt-get install git -y
+apt-get install g++-4.8 -y
+apt-get install libboost-all-dev -y
+apt-get install flex -y
+apt-get install bison -y
+apt-get install libnuma-dev -y
+apt-get install subversion -y
