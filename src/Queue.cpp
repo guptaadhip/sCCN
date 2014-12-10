6ca486000b82b1e52fffedeca5386d98be7791cc
Adhip Gupta <adhipgup@usc.edu>
2014-12-10 02:48:13 -0800

    Resolving make errors

--- src/Queue.cpp
+++ src/Queue.cpp
@@ -1,14 +1,7 @@
 #include "include/PacketHandler.h"
+#include "include/Queue.h"
 
 void Queue::queuePacket(PacketEntry *t) {
-  PacketEntry *stale_head = packet_in_queue_.load(std::memory_order_relaxed);
-  do { 
-    t->next = stale_head;
-  } while (!packet_in_queue_.compare_exchange_weak
-            (stale_head, t, std::memory_order_release));
-
-  if( !stale_head ) { 
-    std::unique_lock<std::mutex> lock(packet_ready_mutex_);
-    packet_ready_.notify_one();
-  }
+  packet_in_queue_.push(t);
+  packet_ready_.notify_one();
 }
